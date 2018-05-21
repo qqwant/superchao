615bb043cb0b9f69b8a21dbcc1a2f868181673c4
qqwant <31196534+qqwant@users.noreply.github.com>
2018-05-21 15:23:21 +0800

    Update label encoding和价值矩阵.py

--- nlp/fasttext/label encoding和价值矩阵.py
+++ nlp/fasttext/label encoding和价值矩阵.py
@@ -6,3 +6,15 @@ list(let.classes_)
 y_true_b=let.transform(xtt['大标签'])
 y_pred_b=let.transform(xtt['big'])
 print (classification_report(y_true_b,y_pred_b,target_names=list(let.classes_)))
+
+#平均划分测试集，保证分布一致，‘y’的分布保证一致
+from sklearn.model_selection import StratifiedShuffleSplit
+import numpy as np
+import numpy as np
+X = np.array(df)
+y = np.array(df["y"])
+#分成2组，测试比例为0.25，训练比例是0.75
+ss=StratifiedShuffleSplit(n_splits=2,test_size=0.01,train_size=0.99,random_state=0)
+
+for train_index, test_index in ss.split(X, y):
+    X_train, X_test = pd.DataFrame(X[train_index]), pd.DataFrame(X[test_index])
